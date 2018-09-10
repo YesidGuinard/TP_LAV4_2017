3301631a67257918c974e2572c8999678349cfbd
Mauricio Cerizza <mauriciocerizza@gmail.com>
2018-09-09 21:52:08 -0300

    login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -17,6 +17,7 @@ export class LoginComponent implements OnInit {
   progresoMensaje="esperando..."; 
   logeando=true;
   ProgresoDeAncho:string;
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
   clase="progress-bar progress-bar-info progress-bar-striped ";
 
